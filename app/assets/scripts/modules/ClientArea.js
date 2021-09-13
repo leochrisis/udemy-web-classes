e5999b1a8a8b093dd7b6c827f626ef79cb34050c
Leonardo Christian <leonardo.chfc@gmail.com>
2021-09-13 12:30:04 -0300

    Add client area feature

---
+++ app/assets/scripts/modules/ClientArea.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+class ClientArea {
+    constructor() {
+        this.injectHTML()
+        this.form = document.querySelector(".client-area__form")
+        this.field = document.querySelector(".client-area__field")
+        this.contentArea = document.querySelector(".client-area__content-area")
+        this.events()
+    }
+
+    events() {
+        this.form.addEventListener("submit", e => {
+            e.preventDefault()
+            this.sendRequest()
+        })
+    }
+
+    sendRequest() {
+        axios.post('https://xenodochial-cray-800740.netlify.app/.netlify/functions/client-area', {password: this.field.value}).then(response =>{
+            this.form.remove()
+            this.contentArea.innerHTML = response.data
+        }).catch(() => {
+            this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not corret. Try again.</p>`
+            this.field.value = ''
+            this.field.focus()
+        })
+    }
+
+    injectHTML() {
+        document.body.insertAdjacentHTML('beforeend', `
+        <div class="client-area">
+            <div class="wrapper wrapper--medium">
+            <h2 class="section-title section-title--blue">Client Area</h2>
+            <form class="client-area__form" action="">
+                <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
+                <button class="btn btn--orange">Submit</button>
+            </form>
+            <div class="client-area__content-area"></div>
+            </div>
+        </div>
+        `)
+    }
+}
+
+export default ClientArea
