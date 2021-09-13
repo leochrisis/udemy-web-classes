85fe98192748bc24edcf03d666ae93397ae3f89a
Leonardo Christian <leonardo.chfc@gmail.com>
2021-09-13 11:47:12 -0300

    Update cloud function to verify password data

--- cloud-functions/client-area.js
+++ cloud-functions/client-area.js
@@ -1,6 +1,25 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the client area"
-    })
+    const clientArea = `
+    <h3> Welcome to the client area</h3>
+    <p>Here you can access all <strong>logged client</strong> benefits</p>
+    `
+    
+    let body
+
+    if(event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if(body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: clientArea
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
 }
